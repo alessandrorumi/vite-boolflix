5edf5632c2095d7a06e10e6df3d3f4240901de78
alessandrorumi <rumi.alessandro21@gmail.com>
2024-01-10 13:33:14 +0100

    Add: AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<template>
+  <header>
+    <div class="title">
+      <h1>BoolFlix</h1>
+    </div>
+    <div class="search">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Ricerca" aria-label="Recipient's username"
+          aria-describedby="button-addon2">
+        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Cerca</button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+
+header {
+  height: 60px;
+  padding: 0 2rem;
+  background-color: #000;
+  @include vert-align-center;
+  justify-content: space-between;
+}
+</style>
