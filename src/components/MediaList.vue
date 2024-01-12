987ba796b9dd4f774bc53ba63a0b19d15768d0d5
alessandrorumi <rumi.alessandro21@gmail.com>
2024-01-12 17:40:23 +0100

    Style: Fixed Header & Series div

--- src/components/MediaList.vue
+++ src/components/MediaList.vue
@@ -16,11 +16,11 @@ export default {
 </script>
 
 <template>
-  <div class="container p-0 pb-5">
+  <div class="container p-0 pt-1 pb-5">
 
     <!-- Film -->
     <h1 class="text-center" v-if="store.movies.results">Film</h1>
-    <div class="row row-cols-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-4  g-3 d-flex flex-wrap justify-content-center">
+    <div class="row row-cols-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-4 g-3 d-flex flex-wrap justify-content-center">
       <div class="my-card text-center" v-for="mediaItem in store.movies.results" :key="mediaItem.id">
         <SingleMedia :mediaInfo="mediaItem" />
       </div>
@@ -28,7 +28,7 @@ export default {
 
     <!-- Serie TV -->
     <h1 class="mt-5 text-center" v-if="store.series.results">Serie TV</h1>
-    <div class="d-flex flex-wrap justify-content-center">
+    <div class="row row-cols-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-4 g-3 d-flex flex-wrap justify-content-center">
       <div class="my-card text-center" v-for="mediaItem in store.series.results" :key="mediaItem.id">
         <SingleMedia :mediaInfo="mediaItem" />
       </div>
